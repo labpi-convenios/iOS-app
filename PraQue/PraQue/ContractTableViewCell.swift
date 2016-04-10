026548fe01941a6e8797e957dd01c39b89bc3ece
Ludimila <lbelacruz@gmail.com>
2016-04-10 11:09:21 -0300

    Alterando storyboard de referencia da tela inicial

--- PraQue/PraQue/ContractTableViewCell.swift
+++ PraQue/PraQue/ContractTableViewCell.swift
@@ -11,6 +11,7 @@ import UIKit
 class ContractTableViewCell: UITableViewCell {
 
     @IBOutlet weak var contractName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
