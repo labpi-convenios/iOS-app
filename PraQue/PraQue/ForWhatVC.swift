55ca1f71a6cd347dc48c1f6320d5458148e13d52
Marcelo <marceloabk@hotmail.com>
2016-04-13 14:05:37 -0300

    Tela pra quê?
    falta mudar as perguntas padrões e cores

--- PraQue/PraQue/ForWhatVC.swift
+++ PraQue/PraQue/ForWhatVC.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ForWhatVC: UIViewController {
+class ForWhatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let questions = ["Questão 1", "Questão 2", "Questão 3"]
 
     @IBAction func cancelButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -16,6 +18,42 @@ class ForWhatVC: UIViewController {
     
     @IBAction func doneButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questions.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! ForWhatTableViewCell
+        
+        cell.questionLabel.text = questions[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let actionSheetController: UIAlertController = UIAlertController(title: "Opções", message: "Selecione a opção", preferredStyle: .ActionSheet)
+        
+        //cancelar a ação
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancelar", style: .Cancel) { action -> Void in
+        }
+        actionSheetController.addAction(cancelAction)
+        
+        //chama a funcao share
+        let shareAction: UIAlertAction = UIAlertAction(title: "Enviar Pergunta ao responsável", style: .Default) { action -> Void in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        actionSheetController.addAction(shareAction)
+        
+        //Present the AlertController
+        self.presentViewController(actionSheetController, animated: true, completion: nil)
     }
     
 }
