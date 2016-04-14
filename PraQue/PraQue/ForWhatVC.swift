a5a83ac0391d3330adaf741ef9d8ff84bd424ccd
Marcelo <marceloabk@hotmail.com>
2016-04-14 00:33:44 -0300

    Finalizando tela "Pra quê?"
    Ainda falta mudar as cores da tela

--- PraQue/PraQue/ForWhatVC.swift
+++ PraQue/PraQue/ForWhatVC.swift
@@ -10,7 +10,35 @@ import UIKit
 
 class ForWhatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let questions = ["Questão 1", "Questão 2", "Questão 3"]
+    var actionSheetController = UIAlertController()
+    
+    
+    let questions = [
+        "Não tenho como saber se a proposta foi realizada",
+        "Não sei o porquê dessa proposta"
+    ]
+    
+    override func viewDidLoad() {
+        self.createActionSheet()
+    }
+    
+    func createActionSheet() {
+        
+        let message = "Ao confirmar será enviada uma solicitação de esclarecimento ao orgão responsável"
+        self.actionSheetController = UIAlertController(title: "Opções", message: message, preferredStyle: .ActionSheet)
+        
+        //cancelar a ação
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancelar", style: .Cancel) { action -> Void in
+        }
+        self.actionSheetController.addAction(cancelAction)
+        
+        //chama a funcao share
+        let shareAction: UIAlertAction = UIAlertAction(title: "Enviar Pergunta ao responsável", style: .Default) { action -> Void in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        self.actionSheetController.addAction(shareAction)
+    }
 
     @IBAction func cancelButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -25,31 +53,16 @@ class ForWhatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! ForWhatTableViewCell
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Question", forIndexPath: indexPath) as! ForWhatTableViewCell
         
         cell.questionLabel.text = questions[indexPath.row]
         
         return cell
-        
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let actionSheetController: UIAlertController = UIAlertController(title: "Opções", message: "Selecione a opção", preferredStyle: .ActionSheet)
-        
-        //cancelar a ação
-        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancelar", style: .Cancel) { action -> Void in
-        }
-        actionSheetController.addAction(cancelAction)
-        
-        //chama a funcao share
-        let shareAction: UIAlertAction = UIAlertAction(title: "Enviar Pergunta ao responsável", style: .Default) { action -> Void in
-            
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }
-        actionSheetController.addAction(shareAction)
-        
-        //Present the AlertController
-        self.presentViewController(actionSheetController, animated: true, completion: nil)
+        self.presentViewController(self.actionSheetController, animated: true, completion: nil)
     }
     
 }
